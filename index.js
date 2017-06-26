0ff00413ab77aada71c2a491c674719266559547
Faisal KANOUT <f.kanout@gmail.com>
2017-06-26 21:08:05 +0200

    Init & solve

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const data = require('./data.js');
+const args = process.argv;
+
+const getSecretKey = function(endpoint){
+  const objectFunction  = { 
+    getKeyByIndex: function (n) {
+      return this[Object.keys(this)[n]];
+    } 
+  }
+  const keys = Object.assign({}, data.keys, objectFunction);
+  return keys.getKeyByIndex([data.endpoints.indexOf(endpoint)])
+
+};
+
+
+if(args.length > 2){
+  let endpoints = args.slice(2, args.length);
+  for(let i in endpoints){
+    console.log(getSecretKey(endpoints[i]));
+  };
+}
+else{
+  console.log('you must specify an endpoint');
+  process.exit(0);
+}
+
+
+
+
+
+
+
+
+
